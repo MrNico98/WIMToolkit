1600113ad2b4cf2804fcd9b1656589f225cb4cf3
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2024-01-17 10:02:26 +0100

    Add files via upload

---
+++ WIMtoolkitDowload.ps1
@@ -0,0 +1,28 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "WIMToolkit necessita di essere lanciato come Amministratore. Riavvio."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm -Uri https://github.com/MrNico98/WIMToolkit/archive/refs/heads/main.zip | iex"
+    break
+}
+
+Remove-Item -Path "C:\WIMToolkit" -Recurse -Force | out-null
+
+# Change to the desktop directory
+cd "C:\"
+
+# Download the script from GitHub
+Invoke-WebRequest -Uri "https://github.com/MrNico98/WIMToolkit/archive/refs/heads/main.zip" -OutFile "WIMToolkit-main.zip"
+
+
+Expand-Archive -Path "WIMToolkit-main.zip" -DestinationPath "." -Force
+
+
+Move-Item -Path "WIMToolkit-main\WIMToolkit" -Destination "WIMToolkit" -Force
+
+
+Remove-Item -Path "WIMToolkit-main" -Recurse -Force
+
+
+Remove-Item -Path "WIMToolkit-main.zip" -Force
+
+
+Start-Process -FilePath ".\WIMToolkit\WIMToolkit.bat"
